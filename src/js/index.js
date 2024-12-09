e0c511ae23ca0c285d8c584d824bc095c61efea3
BeksStupid <beksultanakkuly@gmail.com>
2024-12-09 17:53:05 +0500

    Added all files

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+const showTab = (elTabBtn) => {
+    const elTab = elTabBtn.closest('.tab');
+    if (elTabBtn.classList.contains('tab-btn-active')) {
+        return;
+    }
+    const targetId = elTabBtn.dataset.targetId;
+    const elTabPane = elTab.querySelector(`.tab-pane[data-id="${targetId}"]`);
+    if (elTabPane) {
+        const elTabBtnActive = elTab.querySelector('.tab-btn-active');
+        elTabBtnActive.classList.remove('tab-btn-active');
+        const elTabPaneShow = elTab.querySelector('.tab-pane-show');
+        elTabPaneShow.classList.remove('tab-pane-show');
+        elTabBtn.classList.add('tab-btn-active');
+        elTabPane.classList.add('tab-pane-show');
+    }
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target && !e.target.closest('.tab-btn')) {
+        return;
+    }
+    const elTabBtn = e.target.closest('.tab-btn');
+    showTab(elTabBtn);
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+});
+
+document.querySelector('.home__btn').onclick = function () {
+    document.querySelector('.overlay').style.display = 'flex'
+}
+document.querySelector('.popup__close').onclick = function () {
+    document.querySelector('.overlay').style.display = 'none'
+}
+document.querySelector('.overlay').onclick = function (event) {
+    if (event.target.classList.contains('overlay'))
+    document.querySelector('.overlay').style.display = 'none'
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+
+
+
